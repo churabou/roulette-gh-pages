fd9dde96506fb9186d5d8c2840f1742cbc948d98
churabou <ginga7888@gmail.com>
2021-01-11 16:51:34 +0900

    Updates

---
+++ _next/static/N_4_ooUzsABXQ58VDjyXe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],"/roulette":["static\u002Fchunks\u002Fpages\u002Froulette-5f95ff84a99893183e44.js"],sortedPages:["\u002F_app","\u002F_error","\u002Froulette"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "df766bf9cc4009b2b87b";
+/******/ 	var hotCurrentHash = "cf0a85898a10554e175a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
